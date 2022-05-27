daeb7a49e0162161167ab30c3568b35ac059873a
NUMBART <subhrangshu.pandey.hit@gmail.com>
2022-05-28 00:50:03 +0530

    changed text

--- src/components/RatingPage.js
+++ src/components/RatingPage.js
@@ -4,7 +4,13 @@ export default function RatingPage() {
   return (
     <Grid>
       <Paper elevation={10} style={{ padding: 20, minHeight: '100vh' }}>
-        <Typography>Coming Soon !</Typography>
+        <Typography>
+          Create custom friend leaderboards for{' '}
+          <a href='https://leetcode.com/contest/globalranking/'>
+            global rankings
+          </a>{' '}
+          as well along with country wise rankings. Coming soon!
+        </Typography>
       </Paper>
     </Grid>
   );
